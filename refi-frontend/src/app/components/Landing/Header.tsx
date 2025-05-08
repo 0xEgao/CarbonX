67c590c1d5052a197c837278d9afe32020905315
dodaa08 <dodakartik26@gmail.com>
2025-05-08 19:22:47 +0530

    init

--- refi-frontend/src/app/components/Landing/Header.tsx
+++ refi-frontend/src/app/components/Landing/Header.tsx
@@ -15,6 +15,12 @@ const Header = () => {
   const [alertMessage, setAlertMessage] = useState<string | null>(null);
   const [sortBy, setSortBy] = useState<'recent' | 'price-asc' | 'price-desc' | 'volume-desc'>('recent');
 
+  const handleAlert = (feature: string) => {
+    setAlertMessage(`The Feature Will be Live soon!`);
+    setTimeout(() => {
+      setAlertMessage(null); // Hide alert after 3 seconds
+    }, 3000); // You can adjust this timeout duration
+  };
 
   return (
     <div className="border-2 border-gray-900 rounded-full py-5 px-10 w-[800px] mx-auto text-white">
@@ -31,13 +37,12 @@ const Header = () => {
         {/* Navigation Items */}
         <div className="flex gap-10">
           <h1 className="text-xl font-sans cursor-pointer transition duration-1000">Home</h1>
-          <Link href="/register">
           <h1
             className="text-xl font-sans cursor-pointer"
+            onMouseEnter={() => handleAlert('Register Org')}
           >
             Register Org
-            </h1>
-            </Link>
+          </h1>
           <Link href="/marketplace">
           <h1
             className="text-xl font-sans cursor-pointer"
